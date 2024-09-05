7087492075f90d1bb9075388c20cefef960d4581
Victor Black <victor.domingos.menezes@escola.pr.gov.br>
2024-09-05 03:46:43 +0000

    vitny

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 function setup() {
     createCanvas(600, 600);
-  background ("black");
-  }
-  
-  function draw() {
-   stroke("blue");
-   fill("red");
-    if(mouseIsPressed){
-      rect(mouseX,mouseY,20,35);
-    }
-  }
+      background("black");
+      }
+
+      function draw() {
+        stroke("brown");
+          fill("red");
+            
+                 if(mouseIsPressed) {
+                      rect(mouseX, mouseY, 20, 35);
+                        }
+                        }
